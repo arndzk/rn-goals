226ac3ee28b48d104ed764310a2e2376c122868b
Arnold Dziudzik <arn.dziudzik@gmail.com>
2021-01-27 20:19:40 +0200

    core components and styles

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello, React Native!</Text>
+      <View>
+        <TextInput
+          placeholder="Enter goal..."
+          style={styles.textInput}
+        ></TextInput>
+        <Button title="Add" color="#e96d49" />
+      </View>
+      <View></View>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +21,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 50,
+    backgroundColor: '#121212',
+  },
+  textInput: {
+    borderBottomColor: 'white',
+    borderBottomWidth: 1,
+    color: '#ffffff',
+    marginVertical: 5,
   },
 });

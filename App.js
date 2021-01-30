2f634bfc7061f7fe5a64fc4e2add627e0a849f8c
Arnold Dziudzik <arn.dziudzik@gmail.com>
2021-01-30 19:46:15 +0200

    basic state (hooks) and events

--- App.js
+++ App.js
@@ -1,19 +1,39 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
+  const [enteredGoal, setEnteredGoal] = useState('');
+  const [goals, setGoals] = useState([]);
+  const handleChangeText = (enteredText) => {
+    setEnteredGoal(enteredText);
+  };
+  const addGoalHandler = () => {
+    setGoals((currentGoals) => [...currentGoals, enteredGoal]);
+    setEnteredGoal('');
+  };
   return (
     <View style={styles.container}>
-      <View>
+      <View style={styles.inputWrapper}>
         <TextInput
           placeholder="Enter goal..."
+          placeholderTextColor="#b3b3b3"
+          selectionColor="#e96d49"
           style={styles.textInput}
+          onChangeText={handleChangeText}
+          value={enteredGoal}
         ></TextInput>
-        <Button title="Add" color="#e96d49" />
+        <Button title="Add" color="#e96d49" onPress={addGoalHandler} />
       </View>
-      <View></View>
-      <StatusBar style="auto" />
+      <View>
+        {goals.map((goal) => {
+          return (
+            <View key={goal} style={styles.listItem}>
+              <Text style={styles.listItemText}>{goal}</Text>
+            </View>
+          );
+        })}
+      </View>
     </View>
   );
 }
@@ -25,9 +45,26 @@ const styles = StyleSheet.create({
     backgroundColor: '#121212',
   },
   textInput: {
-    borderBottomColor: 'white',
+    borderBottomColor: '#e96d49',
     borderBottomWidth: 1,
     color: '#ffffff',
     marginVertical: 5,
+    width: '80%',
+  },
+  inputWrapper: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  listItem: {
+    marginTop: 5,
+    marginBottom: 5,
+    padding: 10,
+    backgroundColor: '#282828',
+    borderRadius: 5,
+    color: '#ffffff',
+  },
+  listItemText: {
+    color: '#ffffff',
   },
 });

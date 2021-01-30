9bd79ce4afa3b1e2bad1f23c61bf3705452de8e5
Arnold Dziudzik <arn.dziudzik@gmail.com>
2021-01-30 20:05:07 +0200

    scrollable FlatList

--- App.js
+++ App.js
@@ -1,6 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Button,
+  FlatList,
+  StatusBar,
+} from 'react-native';
 
 export default function App() {
   const [enteredGoal, setEnteredGoal] = useState('');
@@ -9,11 +16,15 @@ export default function App() {
     setEnteredGoal(enteredText);
   };
   const addGoalHandler = () => {
-    setGoals((currentGoals) => [...currentGoals, enteredGoal]);
+    setGoals((currentGoals) => [
+      ...currentGoals,
+      { key: Math.random().toString(), value: enteredGoal },
+    ]);
     setEnteredGoal('');
   };
   return (
     <View style={styles.container}>
+      <StatusBar></StatusBar>
       <View style={styles.inputWrapper}>
         <TextInput
           placeholder="Enter goal..."
@@ -25,15 +36,16 @@ export default function App() {
         ></TextInput>
         <Button title="Add" color="#e96d49" onPress={addGoalHandler} />
       </View>
-      <View>
-        {goals.map((goal) => {
-          return (
-            <View key={goal} style={styles.listItem}>
-              <Text style={styles.listItemText}>{goal}</Text>
-            </View>
-          );
-        })}
-      </View>
+      <FlatList
+        style={{ padding: 10 }}
+        keyExtractor={(item, index) => item.key}
+        data={goals}
+        renderItem={(itemData) => (
+          <View style={styles.listItem}>
+            <Text style={styles.listItemText}>{itemData.item.value}</Text>
+          </View>
+        )}
+      />
     </View>
   );
 }
@@ -41,26 +53,26 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 50,
     backgroundColor: '#121212',
   },
   textInput: {
     borderBottomColor: '#e96d49',
     borderBottomWidth: 1,
     color: '#ffffff',
-    marginVertical: 5,
     width: '80%',
   },
   inputWrapper: {
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
+    backgroundColor: '#181818',
+    padding: 10,
   },
   listItem: {
     marginTop: 5,
     marginBottom: 5,
     padding: 10,
-    backgroundColor: '#282828',
+    backgroundColor: '#404040',
     borderRadius: 5,
     color: '#ffffff',
   },
